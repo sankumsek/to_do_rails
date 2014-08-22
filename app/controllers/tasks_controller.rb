3c7aba00547eb4ac39c275f26234de5d0aaa97ef
Matt Waechter & Sanjeev Sekar <student@epicodus.com>
2014-08-22 11:57:24 -0700

    It will now edit previous entries and display new ones

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,37 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     render('tasks/show.html.erb')
   end
+
+  def new
+    @task = Task.new
+    render('tasks/new.html.erb')
+  end
+
+  def create
+    @task = Task.new(:name => params[:name],
+                     :status => params[:status])
+    if @task.status == nil
+      @task.status = false
+    end
+    if @task.save
+      render('tasks/success.html.erb')
+    else
+      render('tasks/new.html.erb')
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    render('tasks/edit.html.erb')
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(:name => params[:name],
+                    :status => params[:status])
+      render('tasks/success.html.erb')
+    else
+      render('tasks/edit.html.erb')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   match('tasks', {:via => :get, :to => 'tasks#index'})
+  match('tasks/new', {:via => :get, :to => 'tasks#new'})
+  match('tasks', {:via => :post, :to => 'tasks#create'})
   match('tasks/:id', {:via => :get, :to => 'tasks#show'})
+  match('tasks/:id/edit', {:via => :get, :to => 'tasks#edit'})
+  match('tasks/:id', {:via => [:patch, :put], :to => 'tasks#update'})
 end
 
